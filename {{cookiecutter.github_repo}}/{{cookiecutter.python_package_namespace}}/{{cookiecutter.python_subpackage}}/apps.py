4f4bf85d5cc84f3d34d3e43d1d06eb6c3967eefc
Brian Grant <brian.grant@urbanairship.com>
2019-04-30 16:45:27 -0700

    add apps.py

--- {{cookiecutter.github_repo}}/{{cookiecutter.python_package_namespace}}/{{cookiecutter.python_subpackage}}/apps.py
+++ {{cookiecutter.github_repo}}/{{cookiecutter.python_package_namespace}}/{{cookiecutter.python_subpackage}}/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class {{ cookiecutter.python_subpackage|title|replace(' ','' }}Config(AppConfig):
+class {{ cookiecutter.python_subpackage|title|replace(' ','') }}Config(AppConfig):
     name = '{{cookiecutter.python_subpackage}}'
